d6fc5edb647cb3c0d3ac6fab2af756954162dffe
Aaron Wald <aaronwald@gmail.com>
2018-10-20 17:27:29 -0400

    learning

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use std::io;
+use std::io::Read;
+use std::fs::File;
+
 fn do_ref (x:&str) {
     println!("{}", x);
 }
 
 #[derive(Debug)]
+#[allow(dead_code)]
 enum SocketState {
     Open,
     Stream
@@ -42,7 +47,16 @@ fn get_value (state: SocketState) -> u32 {
     }
 }
 
-static LANG: &'static str = "lkajskldjaskld";
+static LANG: &'static str = "lkajskldjaskld"; // not inlined. one memory location
+const CLANG: i32 = 11; // inlined
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+}
 
 fn main() {
     let closure_annotated = |&i| -> i32 { i + 1 };
@@ -57,4 +71,19 @@ fn main() {
 
     let d = Dlaw { name: LANG, width: 10 };
     println!("Dlaw check {}", d.check());
+
+    let f = File::open("hello.txt");
+
+    match read_username_from_file() {
+        Ok(s) => { println!("OK w/ Match {}", s) } ,
+        Err(error) => { panic!("Panic {}", error) }
+    }
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("There was a problem opening the file: {:?}", error)
+        },
+    };
 }
+
